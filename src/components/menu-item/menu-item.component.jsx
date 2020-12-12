ecf0e640d541a3eb0b70241f6ac9135d99176627
Vitalii <vitalii.bondarenko.ua@gmail.com>
2020-12-12 17:10:06 +0200

    added some styles effects to home page

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,13 @@ import './_menu.item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      style={{
-        backgroundImage: `url(${imageUrl}`,
-      }}
-      className={cx('menu-item', { large: !!size })}
-    >
+    <div className={cx('menu-item', { large: !!size })}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl}`,
+        }}
+      />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">Shop now</span>
